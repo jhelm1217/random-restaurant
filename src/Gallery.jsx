7cabc976b2a9c88af624b41f95283fe78cbd8d43
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-04-26 20:33:25 -0400

    updated gallery images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -9,9 +9,9 @@ function Gallery() {
         <div className="p-5" style={{ backgroundColor: 'rgba(0, 0, 0, 0.75)', color: 'white', padding: '20px' }}>
             <Link to='/' style={{ color: 'white', textDecoration: 'none', display: 'inline-block', padding: '10px 20px', backgroundColor: 'purple', borderRadius: '5px', marginBottom: '20px' }}>{'<- Back'}</Link>
             <div className="gallery" tyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img src={purpDrink} alt="Image 1" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
-                <img src={houstonBar} alt="Image 2" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
-                <img src={bathroom} alt="Image 3" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
+                <img src= {purpDrink} alt="Image 1" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
+                <img src ={houstonBar} alt="Image 2" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
+                <img src = {bathroom} alt="Image 3" className="gallery-item" style ={{ width: '50%', height: 'auto' }}/>
 
             </div>
          </div> 
